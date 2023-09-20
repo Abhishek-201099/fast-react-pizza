3952e90addcb30836f7eb89ae17e915816c68613
Abhishek Bhattacharjee <ahishek1250@gmail.com>
2023-09-20 17:58:23 +0530

    Implemented geoLocation feature to get position coordinates and address via reverse geocoding. Added get position button in the createOrder form. Made use of thunk creator from redux toolkit.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart, getTotalCartPrice } from "../cart/cartSlice";
 import EmptyCart from "../cart/EmptyCart";
 import store from "../../store";
-import { getUsername } from "../user/userSlice";
+import { fetchAddress } from "../user/userSlice";
 import { formatCurrency } from "../../utils/helpers";
 
 // https://uibakery.io/regex-library/phone-number
@@ -16,18 +16,26 @@ const isValidPhone = (str) =>
   );
 
 function CreateOrder() {
+  const dispatch = useDispatch();
   const [withPriority, setWithPriority] = useState(false);
-  const username = useSelector(getUsername);
+  const {
+    username,
+    status: addressStatus,
+    address,
+    position,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
   const totalCartPrice = useSelector(getTotalCartPrice);
-  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
-  const totalPrice = totalCartPrice + priorityPrice;
+  const cart = useSelector(getCart);
   // Here we have used useNavigation() to check the form submission status.
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
-
   // useActionData() will return the data that we have returned from the action.
   const formErrors = useActionData();
-  const cart = useSelector(getCart);
+
+  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
+  const totalPrice = totalCartPrice + priorityPrice;
+  const isLoadingAddress = addressStatus === "loading";
+  const isSubmitting = navigation.state === "submitting";
 
   if (!cart.length) return <EmptyCart />;
 
@@ -63,15 +71,36 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               className="input w-full"
               type="text"
               name="address"
+              defaultValue={address}
+              disabled={isLoadingAddress}
               required
             />
+            {addressStatus === "error" && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {errorAddress}
+              </p>
+            )}
+            {!position.latitude && !position.longitutde && (
+              <span className="absolute right-[5px] top-[31px] z-50 mt-1 md:right-[5px] md:top-[1px]">
+                <Button
+                  type="small"
+                  disabled={isLoadingAddress}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    dispatch(fetchAddress());
+                  }}
+                >
+                  get position
+                </Button>
+              </span>
+            )}
           </div>
         </div>
 
@@ -92,8 +121,17 @@ function CreateOrder() {
         <div>
           {/* We've used type='hidden' because we want this cart value to be sent upon submission as well without showing it on UI. */}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <input
+            type="hidden"
+            name="position"
+            value={
+              position.longitude && position.latitude
+                ? `${position.latitude},${position.longitude}`
+                : ""
+            }
+          />
           <Button type="primary" disabled={isSubmitting}>
-            {isSubmitting
+            {isSubmitting || isLoadingAddress
               ? "Placing order..."
               : `Order now for ${formatCurrency(totalPrice)}`}
           </Button>
